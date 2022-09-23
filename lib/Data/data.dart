e6014c2e3fc713307623b929a8ca1331c2050a9f
adebayoAdesina <adebayoadesina789@gmail.com>
2022-09-23 14:42:35 +0100

    api hidden.

--- lib/Data/data.dart
+++ lib/Data/data.dart
@@ -2,13 +2,14 @@ import 'dart:convert';
 
 import 'package:http/http.dart' as http;
 import 'package:weather_app/Model/weather.dart';
+import './api_key.dart';
 
 class GetWeathers {
   List<WeatherModel> searchedWeather = [];
   
   Future<void> getWeather(String city) async {
     String url =
-        'http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=c78c6aeb3122c76d167e1022b0dfb944';
+        'http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${FLUTTER_APP_WEATHER_API}';
 
     var response = await http.get(Uri.parse(url));
     Map<dynamic, dynamic> value = jsonDecode(response.body);
